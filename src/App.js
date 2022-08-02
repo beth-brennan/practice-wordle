b65f16afd9fd74374f9b356f2692bcef9377a1b2
Beth Brennan <bethbrennan2@gmail.com>
2022-08-01 22:34:47 -0400

    added styling on input, fixed bug when hitting backspace, ended puzzle when won

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     const [fieldName, fieldIndex] = name.split("-");
 
     let guess = [...currentGuess];
-    guess[fieldIndex] = value;
+    guess[fieldIndex] = value.toUpperCase();
     setCurrentGuess(guess);
 
     if (value.length === 1) {
@@ -38,9 +38,33 @@ function App() {
     }
   }
 
+  const handleKeyDown = (event) => {
+    console.log("keydown pressed")
+    const { name, value } = event.target;
+    const [fieldName, fieldIndex] = name.split("-");
+
+    if (event.key === 'Backspace') {
+      if (value === "") {
+        const lastEntry = document.querySelector(
+          `input[name=letter-${parseInt(fieldIndex, 10) - 1}]`
+        );
+        if (lastEntry !== null) {
+          lastEntry.focus();
+        }
+      } else {
+        const nextEntry = document.querySelector(
+          `input[name=letter-${parseInt(fieldIndex, 10) + 1}]`
+        );
+        if (nextEntry !== null) {
+          nextEntry.focus();
+        }
+      }
+    }
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (currentGuess.join("").length === 5 && possibleAnswers.includes(currentGuess.join(""))) {
+    if (currentGuess.join("").length === 5 && possibleAnswers.includes(currentGuess.join("").toLowerCase())) {
       const guess = [...currentGuess];
       let unusedLetters = word.split("");
 
@@ -63,14 +87,18 @@ function App() {
       let newLineArray = [...lineArray];
       let currentIndex = newLineArray.indexOf("current");
       newLineArray[currentIndex] = "past";
-      newLineArray[currentIndex + 1] = "current";
+
+      if (currentGuess.join("") !== word) {
+        newLineArray[currentIndex + 1] = "current";
+      }
+
       setLineArray(newLineArray);
     }
   }
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * possibleAnswers.length);
-    setWord(possibleAnswers[randomIndex]);
+    setWord(possibleAnswers[randomIndex].toUpperCase());
   }, [])
 
   return (
@@ -79,60 +107,75 @@ function App() {
         console.log(word);
         if (line === "current") {
           return (
-            <form key={index} onSubmit={handleSubmit}>
+            <form key={index} onSubmit={handleSubmit} className="rows">
               <input
+                className='input'
                 type="text"
                 name="letter-0"
                 minLength={1}
                 maxLength={1}
                 autoFocus="autofocus"
                 onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                value={currentGuess[0]}
               />
               <input
+                className='input'
                 type="text"
                 name="letter-1"
                 minLength={1}
                 maxLength={1}
                 onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                value={currentGuess[1]}
               />
               <input
+                className='input'
                 type="text"
                 name="letter-2"
                 minLength={1}
                 maxLength={1}
                 onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                value={currentGuess[2]}
               />
               <input
+                className='input'
                 type="text"
                 name="letter-3"
                 minLength={1}
                 maxLength={1}
                 onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                value={currentGuess[3]}
               />
               <input
+                className='input'
                 type="text"
                 name="letter-4"
                 minLength={1}
                 maxLength={1}
                 onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                value={currentGuess[4]}
               />
-              <input type="submit" />
+              <input type="submit" id="submit-button" />
             </form>
           )
         } else if (line === "future") {
           return (
-            <div id='blank-rows' key={index}>
-              <span className='blank'>1</span>
-              <span className='blank'>2</span>
-              <span className='blank'>3</span>
-              <span className='blank'>4</span>
-              <span className='blank'>5</span>
+            <div className="rows" key={index}>
+              <span className='blank' />
+              <span className='blank' />
+              <span className='blank' />
+              <span className='blank' />
+              <span className='blank' />
             </div>
           )
         } else {
           const word = guesses[index];
           return (
-            <div id='answer-rows' key={index}>
+            <div className="rows" key={index}>
               <span className={word[0]["type"]}>
                 {word[0]["letter"]}
               </span>
